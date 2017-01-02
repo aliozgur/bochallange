b53057b88ad8b29ec22ba2674dc6d895a9072d8a
Ali Ozgur <aliozgur79@gmail.com>
2017-01-02 21:43:51 +0300

    Reimplement problem action

--- Controllers/ChallangeController.cs
+++ Controllers/ChallangeController.cs
@@ -38,14 +38,14 @@ namespace bocloudchallange.Controllers
                         ".NET, C#, F#, R, Cloud and Git",
                         "Xamarin"
                     },
-                    /*Blog = "http://aliozgur.net",
+                    Blog = "http://aliozgur.net",
                     Projects = new Dictionary<string,string>(){
                         ["PragmaSql"] = "Open source T-SQL editor",
                         ["Xamarin"] = "Couple of open source plugins for Xamarin Forms",
                         ["Serilog.HttpContextData"] = "Enrich your log messages with HttpContext data",
                         ["SeqApps"] = "Seq apps for Jira, Rocket.Chat, GitLab and GitHub",
                     }
-                    */
+                    
             };
 
             return new JsonResult(myProfile);
@@ -55,22 +55,17 @@ namespace bocloudchallange.Controllers
         [Route("problem")]
         public IActionResult TheProblem(int number)
         {
-            switch (number)
+            var a = 0;
+            var b = 1;
+            while (true)
             {
-                case 2: return new ObjectResult(true);
-                case 3: return new ObjectResult(true);
-                case 4: return new ObjectResult(false);
-                case 5: return new ObjectResult(true);
-                case 6: return new ObjectResult(false);
-                case 7: return new ObjectResult(false);
-                case 8: return new ObjectResult(true);
-                case 9: return new ObjectResult(false);
-                case 10: return new ObjectResult(false);
-                case 11:return new ObjectResult(false);
-                case 12: return new ObjectResult(false);
-                case 13: return new ObjectResult(true);
-                case 14: return new ObjectResult(false);
-                default: return NotFound(  );
+                if (a == number)
+                    return new ObjectResult(true);
+                if (a > number)
+                    return new ObjectResult(false);
+                var t = a;
+                a = b;
+                b = t + b;
             }
         }
        
